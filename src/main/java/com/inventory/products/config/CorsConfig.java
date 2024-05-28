6c57e7f60bf335628c9775a8e2093fe0c1436068
NAREN DUBAN LOPEZ ROMERO <nlopezr6@ucentral.edu.co>
2024-05-27 23:04:43 -0500

    cambio politicas cors

--- src/main/java/com/inventory/products/config/CorsConfig.java
+++ src/main/java/com/inventory/products/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200","https://inventario-front-delta.vercel.app/");
       }
     };
   }
